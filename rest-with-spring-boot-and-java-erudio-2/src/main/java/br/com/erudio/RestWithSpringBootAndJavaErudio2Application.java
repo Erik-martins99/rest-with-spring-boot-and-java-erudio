d59eab6d4a55060d578f9bd644d5c765739e4399
Erik Martins <erik.oliv.martins@gmail.com>
2023-10-31 20:06:54 -0300

    First steps in java with Spring Boot

---
+++ rest-with-spring-boot-and-java-erudio-2/src/main/java/br/com/erudio/RestWithSpringBootAndJavaErudio2Application.java
@@ -0,0 +1,13 @@
+package br.com.erudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithSpringBootAndJavaErudio2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithSpringBootAndJavaErudio2Application.class, args);
+	}
+
+}
